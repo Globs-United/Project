d12e60ec91b82ad97579156d15370fe280fc58f4
ichigojin <ky246.code@gmail.com>
2025-03-29 15:07:35 -0700

    Clean up

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,62 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jump_check = false
-var playerstate = "idle"
-	#idle, run, jump
+@export var accelerationSpeed = 16;
+@export var maxSpeed = 400;
 
+var screen_size;
+var downNow = 1;
 
-#I said screw it and ripped code from my previous game jam since it didn't autofill to something like this, like I what occurred last time
+func _ready():
+	screen_size = get_viewport_rect().size
+	velocity = Vector2.ZERO;
+	
+	accelerationSpeed *= screen_size.y / 700;
+	maxSpeed *= screen_size.y / 700;
 
-func _physics_process(delta: float) -> void:
-	
-	# Add the gravity.  (+update is_in_air)
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		#falling?
-		if velocity.y < 0:
-			playerstate = "jump"
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		playerstate = "jump"
-		
-	#dynamic jump, only letting occur once due to spamming == flying
-	if Input.is_action_just_released("jump") and velocity.y < 0:
-		if not jump_check:
-			velocity.y = JUMP_VELOCITY / 2
-		jump_check = true
-		
-		
-	if is_on_floor() and velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-		playerstate = "run"
-	elif is_on_floor() and velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
-		playerstate = "run"
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED / 2)
-	
-	if velocity.x == 0 and velocity.y == 0:
-		playerstate = "idle"
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("moveLeft", "moveRight")
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	playeranim()
-	move_and_slide()
+func _process(delta):
+	velocity *= 0.9;
+	if Input.is_action_pressed("moveRight"):
+		velocity.x += accelerationSpeed
+	if Input.is_action_pressed("moveLeft"):
+		velocity.x -= accelerationSpeed
+	if Input.is_action_pressed("moveDown"):
+		velocity.y += accelerationSpeed
+	if Input.is_action_pressed("moveUp"):
+		velocity.y -= accelerationSpeed
 
-func playeranim():
-	if playerstate == "idle":
-		$AnimatedSprite2D.play("test")
-	#elif playerstate == "":
-	#	$AnimatedSprite2D.play("")
+	if velocity.length() > maxSpeed:
+		velocity = velocity.normalized() * maxSpeed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var velocityTemp = velocity.bounce(collision_info.get_normal())
+		if(!(velocity.y * downNow > 0 && velocityTemp.y * downNow < 0)):
+			velocity = velocityTemp;
--- wall.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-
-var screen_size;
-
-func _ready():
-	screen_size = get_viewport_rect().size
-	
-	position *= screen_size.y / 700;
-	scale    *= screen_size.y / 700;
