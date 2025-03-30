1626489dba92ffda181de4adadae9ba8631b79cc
Extreme-Kitten <sgbbgold@gmail.com>
2025-03-30 16:09:27 -0700

    lightning pt2/2

--- scripts/lightning_cloud.gd
+++ scripts/lightning_cloud.gd
@@ -4,12 +4,17 @@ extends StaticBody2D
 var lightning_striking = false
 var player_within = false
 @export var Yworld = false
+@export var strike_time = 1.0
+@export var rest_time = 1.0
 
 
 func _ready() -> void:
 	if Yworld:
 		change_world(false);
+	$RestTimer.wait_time = rest_time
+	$StrikeTimer.wait_time = strike_time
 	$RestTimer.start()
+	
 
 func _process(_delta: float) -> void:
 	if lightning_striking && player_within:
@@ -21,15 +26,12 @@ func _process(_delta: float) -> void:
 		$AnimatedSprite2D.play("blinkY")
 	else:
 		$AnimatedSprite2D.play("blink")
-	
-	if $Lightning/AnimatedSprite2D.frame == 16:
-		_on_animated_sprite_2d_animation_finished();
 
 
 
 
 
-func _on_animated_sprite_2d_animation_finished() -> void:
+func _on_strike_timer_timeout() -> void:
 	$RestTimer.start()
 	$Lightning/AnimatedSprite2D.play("wait")
 	lightning_striking = false
@@ -37,6 +39,7 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 
 func _on_rest_timer_timeout() -> void:
 	lightning_striking = true
+	$StrikeTimer.start()
 	if Yworld:
 		$Lightning/AnimatedSprite2D.play("strikeY")
 	else:
