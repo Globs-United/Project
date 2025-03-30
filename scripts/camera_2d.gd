00d3cfce044862636a2e7b45d27345b6a732632e
ichigojin <ky246.code@gmail.com>
2025-03-30 15:55:14 -0700

    Minor changes to flame script

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,5 +1,8 @@
 extends Camera2D
 
+var screen_size;
+
 func _process(delta: float) -> void:
+	screen_size = get_viewport_rect().size
 	#$".".position.y = get_node("../Player").position.y
-	$".".position.x = get_node("../Player").position.x
+	$".".position.x = max(get_node("../Player").position.x, screen_size.x / 2);
