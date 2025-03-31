998bf302bcee504fcc595f2872b3e7a0e5d4b8bd
ichigojin <ky246.code@gmail.com>
2025-03-30 19:42:22 -0700

    Make some portals one way

--- scripts/pit.gd
+++ scripts/pit.gd
@@ -2,12 +2,19 @@ extends Area2D
 
 var initVel
 var player_within_counter = 0
-var player_within = false
+var player_within = false;
+
+var scene = preload("res://object_scenes/wall.tscn");
+var instance = false;
+
+@export var oneWay = false;
 
 func _process(delta: float) -> void:
 	if player_within:
 		player_within_counter += 1
 		print(player_within_counter)
+	if instance && get_node("../Player").death_lock:
+		instance.free();
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -19,8 +26,15 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Player detected")
-		get_tree().call_group("player", "change_world")
+		get_tree().call_group("player", "change_world");
 		get_tree().call_group("player", "exitting_pit", initVel)
+		if oneWay:
+			instance = scene.instantiate()
+			get_node("../").add_child(instance);
+			
+			instance.position = position;
+			instance.scale = 5 * scale;
+			instance.visible = false;
 	if body.is_in_group("enemy"):
 		get_tree().call_group("enemy", "change_world")
 	print("Body detected! Changing their world")
